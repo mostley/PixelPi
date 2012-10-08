6a1e0abc40ebb9e0b35d8864c2da64557cdb53eb
Mostley <sven.hecht@gmail.com>
2012-10-08 21:39:54 +0000

    Modified pong.py

--- site/modules/pong.py
+++ site/modules/pong.py
@@ -42,6 +42,8 @@ class Pong:
             #print "update",dt,self.ball,self.ballvelocity * dt
             self.ball += self.ballvelocity * dt
             
+            if self.ball.y > PIXEL_DIM_Y
+            
             self.draw()
             
             time.sleep(1/24.0)
@@ -65,13 +67,13 @@ class Pong:
         print "pong - executing command:" + data['command']
         if data['command'] == 'pong_left_up':
             self.leftPaddle.y += 1
-            if self.leftPaddle.y > PIXEL_DIM_X - 1: self.leftPaddle.y = PIXEL_DIM_X - 1
+            if self.leftPaddle.y > PIXEL_DIM_Y - 1: self.leftPaddle.y = PIXEL_DIM_Y - 1
         elif data['command'] == 'pong_left_down':
             self.leftPaddle.y -= 1
             if self.leftPaddle.y < 0: self.leftPaddle.y = 0
         elif data['command'] == 'pong_right_up':
             self.rightPaddle.y += 1
-            if self.rightPaddle.y > PIXEL_DIM_X - 1: self.rightPaddle.y = PIXEL_DIM_X - 1
+            if self.rightPaddle.y > PIXEL_DIM_Y - 1: self.rightPaddle.y = PIXEL_DIM_Y - 1
         elif data['command'] == 'pong_right_down':
             self.rightPaddle.y -= 1
             if self.rightPaddle.y < 0: self.rightPaddle.y = 0
