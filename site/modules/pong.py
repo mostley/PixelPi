23f2a9e17acd6edb80e44927b2f9b82843eee087
Mostley <sven.hecht@gmail.com>
2012-10-08 22:10:59 +0000

    Modified pong.py

--- site/modules/pong.py
+++ site/modules/pong.py
@@ -78,7 +78,7 @@ class Pong:
                     self.ballvelocity.x *= -1
             elif self.ball.x < 1:
                 if int(self.ball.y) == int(self.leftPaddle.y):
-                    self.ball.x = -self.ball.x
+                    self.ball.x = 1-self.ball.x
                     self.ballvelocity.x *= -1
                 
             if self.ball.x > self.maxX:
