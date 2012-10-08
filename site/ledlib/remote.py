f3f93b07a60b5cdc6b8eec4d619db1cdf508403c
Sven Hecht <sven.hecht@gmail.com>
2012-10-08 21:43:05 +0200

    added pong module and module mechanics

---
+++ site/ledlib/remote.py
@@ -0,0 +1,64 @@
+import socket, math
+from vector import *
+
+class Remote:
+
+    def __init__(self):
+        self.UDP_IP = '127.0.0.1'
+        self.UDP_PORT = 6803
+        self.verbose = False
+
+    def sendGrid(self, grid):
+        bytedata = self.toByteArray(grid)
+        self.sendBytes(bytedata)
+
+    def sendBytes(self, bytedata):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto( bytedata, (self.UDP_IP, self.UDP_PORT) )
+
+    def toByteArray(self, grid):
+        result = bytearray(BUFFER_SIZE)
+
+        i = 0
+        while i < BUFFER_SIZE:
+            y = i/3 % PIXEL_DIM_Y
+            x = int(math.floor(i/3 / PIXEL_DIM_Y))
+            if x % 2 == 0:
+                c = grid[x][y]
+                result[i] = c[0]
+                i = i + 1
+                result[i] = c[1]
+                i = i + 1
+                result[i] = c[2]
+                i = i + 1
+            else:
+                c = grid[x][(PIXEL_DIM_Y-1)-y]
+                result[i] = c[0]
+                i = i + 1
+                result[i] = c[1]
+                i = i + 1
+                result[i] = c[2]
+                i = i + 1
+
+        return result
+
+    def createGridBuffer(self, color):
+        result = []
+        for x in range(PIXEL_DIM_X):
+            result.append([])
+            for y in range(PIXEL_DIM_Y):
+                result[x].append(color)
+        return result
+    
+    def setPixel(self, grid, v, color):
+        pos = v.toIntArr()
+        if pos[0] >= 0 and pos[0] < PIXEL_DIM_X and \
+           pos[1] >= 0 and pos[1] < PIXEL_DIM_Y:
+            grid[pos[0]][pos[1]] = color
+
+    def clearPixels(self, grid, color):
+        for x in range(len(grid)):
+            for y in range(len(grid[x])):
+                grid[x][y] = color
+
+
--- site/server.py
+++ site/server.py
@@ -9,84 +9,103 @@ from mako.lookup import TemplateLookup
 
 class Root(object):
 
-	def __init__(self):
-		self.lookup = TemplateLookup(directories=['html'])
+    def __init__(self):
+        self.lookup = TemplateLookup(directories=['html'])
 
-		self.currentMode = 'pong'
+        self.currentMode = 'pong'
+        self.modules = {}
 
-	@cherrypy.expose
-	def index(self):
-		print cherrypy.request.method
-		tmpl = self.lookup.get_template("index.html")
-		return tmpl.render(salutation="Hello", target="World")
+        self.validCommands = []
 
-	@cherrypy.expose
-	@cherrypy.tools.json_out(on = True)
-	def update(self):
-		result = { 'success': False, 'command': '' }
-		cl = cherrypy.request.headers['Content-Length']
-		rawbody = cherrypy.request.body.read(int(cl))
+    @cherrypy.expose
+    def index(self):
+        print cherrypy.request.method
+        tmpl = self.lookup.get_template("index.html")
+        return tmpl.render(salutation="Hello", target="World")
 
-		print "'",rawbody,"'"
+    def setMode(self, mode):
+        self.currentMode = mode
 
-		if len(rawbody) > 0:
-			body = simplejson.loads(rawbody)
-			print "'",body,"'"
+        if not self.modules.has_key(mode):
+            self.modules[mode] = __import__(mode)
 
-			result['command'] = body["command"]
+        if self.modules.has_key(mode):
+            self.currentModule = self.modules[mode]
 
-			if body["command"] == 'status':
-				result['success'] = True
-				result['mode'] = self.currentMode
-			elif body["command"] == 'changemode':
-				self.currentMode = body['mode']
-				result['mode'] = self.currentMode
-			elif body["command"] == 'pong_left_up':
-				result['success'] = True
-			elif body["command"] == 'pong_left_down':
-				result['success'] = True
-			elif body["command"] == 'pong_right_up':
-				result['success'] = True
-			elif body["command"] == 'pong_right_down':
-				result['success'] = True
+            self.currentModule.init()
+            self.validCommands = self.currentModule.commands
 
-		return result
+    def executeCommand(self, data):
+        result = False
+        if self.currentModule:
+            result = self.currentModule.execute(data)
+        return result
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out(on = True)
+    def update(self):
+        result = { 'success': False, 'command': '' }
+        cl = cherrypy.request.headers['Content-Length']
+        rawbody = cherrypy.request.body.read(int(cl))
+
+        print "'",rawbody,"'"
+
+        if len(rawbody) > 0:
+            body = simplejson.loads(rawbody)
+            print "'",body,"'"
+
+            result['command'] = body["command"]
+
+            if body["command"] == 'status':
+                result['success'] = True
+                result['mode'] = self.currentMode
+            
+            elif body["command"] == 'changemode':
+                self.loadModule(body['mode'])
+                result['mode'] = body['mode']
+
+            elif body["command"] in self.validCommands:
+                result['success'] = self.executeCommand(data)
+
+        return result
 
 if __name__ == '__main__':
-	current_dir = os.path.dirname(os.path.abspath(__file__))
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
-	# Set up site-wide config first so we get a log if errors occur.
-	cherrypy.config.update({
-		#'environment': 'production',
-		'log.error_file': 'logs/site.log',
-		'log.screen': True
-	})
+    # Set up site-wide config first so we get a log if errors occur.
+    cherrypy.config.update({
+        #'environment': 'production',
+        #'server.socket_host': 'mifune',
+        #'server.socket_port': 8080,
+        'log.error_file': 'logs/site.log',
+        'log.screen': True
+    })
 
-	conf = {
-		'global': {
-		#	'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-			"request.methods_with_bodies": ("POST", "PUT", "PROPPATCH")
-		},
-		'/update': {
-			"request.methods_with_bodies": ("POST", "GET", "PUT", "PROPPATCH")
-		},
-		'/css': {
-			'tools.staticdir.on': True,
-			'tools.staticdir.dir': os.path.join(current_dir, 'css'),
-			'tools.staticdir.content_types': { 'css': 'text/css' }
-		},
-		'/js': {
-			'tools.staticdir.on': True,
-			'tools.staticdir.dir': os.path.join(current_dir, 'js'),
-			'tools.staticdir.content_types': { 'css': 'text/javascript' }
-		},
-		'/img': {
-			'tools.staticdir.on': True,
-			'tools.staticdir.dir': os.path.join(current_dir, 'img'),
-			'tools.staticdir.content_types': {
-				'png': 'image/png',
-				'jpg': 'image/jpg'
-			}
-		}
-	}
-	cherrypy.quickstart(Root(), '/', config=conf)
+    conf = {
+        'global': {
+        #   'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            "request.methods_with_bodies": ("POST", "PUT", "PROPPATCH")
+        },
+        '/update': {
+            "request.methods_with_bodies": ("POST", "GET", "PUT", "PROPPATCH")
+        },
+        '/css': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': os.path.join(current_dir, 'css'),
+            'tools.staticdir.content_types': { 'css': 'text/css' }
+        },
+        '/js': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': os.path.join(current_dir, 'js'),
+            'tools.staticdir.content_types': { 'css': 'text/javascript' }
+        },
+        '/img': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': os.path.join(current_dir, 'img'),
+            'tools.staticdir.content_types': {
+                'png': 'image/png',
+                'jpg': 'image/jpg'
+            }
+        }
+    }
+    cherrypy.quickstart(Root(), '/', config=conf)
