b44e74ec1658917e2f5fd3f1376567d3304bf4c0
Sven Hecht <sven.hecht@gmail.com>
2013-01-01 14:19:24 +0000

    Modified analyzer.py

--- sound/analyzer.py
+++ sound/analyzer.py
@@ -4,3 +4,13 @@ import pyaudio
 import numpy as np
 import wave
 
+chunk = 2048
+
+window = np.blackman(chunk)
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+p = pyaudio.PyAudio()
+myStream = p.open(format = FORMAT, channels = CHANNELS, rate = RATE, input = True, frames_per_buffer = chunk)
