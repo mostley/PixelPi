8a81f1f732bf37497bab43ae63e6a23a6ebf18df
Sven Hecht <sven.hecht@gmail.com>
2013-01-01 14:47:04 +0000

    Modified analyzer.py

--- sound/analyzer.py
+++ sound/analyzer.py
@@ -22,6 +22,13 @@ myStream = p.open(
 	output = True,
 	frames_per_buffer = chunk)
 
+def Pitch(signal):
+	signal = np.fromstring(signal, 'Int16')
+	crossing = [math.copysign(1.0, s) for s in signal]
+	index = find(np.diff(crossing))
+	f0 = round(len(index) * RATE / ( 2 * np.prod(len(signal))))
+	return f0
+
 for i in range(0, RATE / chunk * RECORD_SECONDS):
 	data = stream.read(chunk)
 	Frequency=Pitch(data)
