cb40dac83f46cf91829e9bce1dd9a5e28148c0f3
Sven Hecht <sven.hecht@gmail.com>
2013-01-01 14:20:44 +0000

    Modified analyzer.py

--- sound/analyzer.py
+++ sound/analyzer.py
@@ -14,3 +14,8 @@ RATE = 44100
 
 p = pyaudio.PyAudio()
 myStream = p.open(format = FORMAT, channels = CHANNELS, rate = RATE, input = True, frames_per_buffer = chunk)
+
+for i in range(0, RATE / chunk * RECORD_SECONDS):
+	data = stream.read(chunk)
+	Frequency=Pitch(data)
+	print "%f Frequency" %Frequency
