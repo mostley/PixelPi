565772102cfaff411e3a93472d752ee0d399fb36
Sven Hecht <sven.hecht@gmail.com>
2013-01-04 09:25:53 +0000

    Modified installdaemon.sh

--- controllcenter/installdaemon.sh
+++ controllcenter/installdaemon.sh
@@ -2,5 +2,5 @@ echo "**** Installing the Controll Center as a service ****"
 echo "**** (to uninstall service, execute: 'sudo update-rc.d -f controllcenter.sh remove') ****"
 cp "/home/pi/pixelpi/controllcenter/controllcenter.sh" "/etc/init.d"
 cd /etc/init.d
-chmod 755 adafruit-webide.sh
+chmod 755 controllcenter.sh
 update-rc.d controllcenter.sh defaults
