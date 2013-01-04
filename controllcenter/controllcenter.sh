8035ddffad71449fcc47816f97b19462cd4cd2a9
Sven Hecht <sven.hecht@gmail.com>
2013-01-04 09:18:58 +0000

    Modified controllcenter.sh

---
+++ controllcenter/controllcenter.sh
@@ -0,0 +1,11 @@
+### BEGIN INIT INFO
+# Provides:          controllcenter
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: start controllcenter daemon at boot time
+# Description:       Enable service provided by daemon
+### END INIT INFO
+
+python /home/pi/pixelpi/controllcenter/controllcenter.py
