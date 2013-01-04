53d0ecd26aa4b21656429f3170035a6aad38fda2
Sven Hecht <sven.hecht@gmail.com>
2013-01-04 09:25:44 +0000

    Modified installdaemon.sh

--- controllcenter/installdaemon.sh
+++ controllcenter/installdaemon.sh
@@ -1,5 +1,5 @@
-echo "**** Installing the WebIDE as a service ****"
-echo "**** (to uninstall service, execute: 'sudo update-rc.d -f adafruit-webide.sh remove') ****"
+echo "**** Installing the Controll Center as a service ****"
+echo "**** (to uninstall service, execute: 'sudo update-rc.d -f controllcenter.sh remove') ****"
 cp "/home/pi/pixelpi/controllcenter/controllcenter.sh" "/etc/init.d"
 cd /etc/init.d
 chmod 755 adafruit-webide.sh
