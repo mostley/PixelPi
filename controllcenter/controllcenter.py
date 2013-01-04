ec39049d339f6788dc2654a78781352cbe517650
Sven Hecht <sven.hecht@gmail.com>
2013-01-04 09:18:53 +0000

    Modified controllcenter.py

--- controllcenter/controllcenter.py
+++ controllcenter/controllcenter.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-### BEGIN INIT INFO
-# Provides:          controllcenter
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: start controllcenter daemon at boot time
-# Description:       Enable service provided by daemon
-### END INIT INFO
-
 import time
 from evdev import *
 
